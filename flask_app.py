d8ad1951d17699ce4d2e1b2d41ef9e9529b24b9c
barrukurniawan <barru.kurniawan@gmail.com>
2021-09-03 02:31:23 +0000

    updated templates, api lofin, register

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,12 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from db import db, app
 from models import User
 import datetime
+import re
 
 sqldb = SQLAlchemy(app)
 # app = Flask(__name__)
@@ -39,11 +40,45 @@ sqldb = SQLAlchemy(app)
 def hello_world():
     return 'Hello from Flask!'
 
+@app.route('/login')
+def login_page():
+    return render_template("register.html")
+
 @app.route('/register', methods=["POST"])
 def create_message():
     register = User()
 
-    print ("FORMNYAAAA")
+    regex_symbol = '[@_!#$%^&*()<>?/\|}{~:[\]]'
+    regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
+
+    if(re.search(regex_symbol, request.json['name'])) or (re.search(regex_symbol, request.json['password'])):
+        response = {
+            "data": "Nama atau password tidak valid",
+            "message": "Tidak boleh mengandung symbol",
+            "status": 'FAILED_REGISTER'
+        }
+        return response
+
+    if(re.search(regex, request.json['email'])):
+        print ("valid email")
+    else:
+        response = {
+            "data": "Email tidak valid",
+            "message": "Cek kembali email anda",
+            "status": 'FAILED_REGISTER'
+        }
+        return response
+
+    check_user = User.query.filter(User.deleted_at == None) \
+                    .filter(User.email == request.json['email']).first()
+
+    if check_user is not None:
+        response = {
+            "data": "Email sudah terdaftar, gunakan Email lain",
+            "message": "Gagal Registrasi",
+            "status": 'FAILED_REGISTER'
+        }
+        return response
 
     register.name = request.json['name']
     register.email = request.json['email']
@@ -51,16 +86,75 @@ def create_message():
     register.created_at = datetime.datetime.now()
     register.updated_at = datetime.datetime.now()
 
-    print ('bismillah')
-    print (register)
-
     db.session.add(register)
     db.session.commit()
 
     response = {
         "data": "berhasil",
         "message": "created user!",
-        "status_code": 200
+        "status": 'SUCCESS_REGISTER'
     }
+
+    return response
+
+@app.route('/list-user', methods=["GET"])
+def list_user():
+    customer = User.query.filter(User.deleted_at == None).all()
+
+    data = []
+    for x in customer:
+        data.append({
+            "username" : x.name,
+            "email" : x.email
+        })
+
+    response = {
+        "data": data,
+        "message": "list user!",
+        "status": 'SUCCESS_USER_LIST'
+    }
+
+    return response
+
+@app.route('/login', methods=["POST"])
+def login():
+    response = {
+        "data": "User's not found",
+        "message": "Email atau Password Anda Salah",
+        "status": 'FAILED_LOGIN'
+    }
+
+    regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
+    regex_symbol = '[@_!#$%^&*()<>?/\|}{~:[\]]'
+
+
+    if(re.search(regex, request.json['email'])):
+        print ("valid email")
+    else:
+        response = {
+            "data": "Email tidak valid",
+            "message": "Cek kembali email anda",
+            "status": 'FAILED_LOGIN'
+        }
+        return response
+
+    if(re.search(regex_symbol, request.json['password'])):
+        response = {
+            "data": "Password tidak valid",
+            "message": "Tidak boleh mengandung symbol",
+            "status": 'FAILED_LOGIN'
+        }
+        return response
+
+    check_user = User.query.filter(User.deleted_at == None) \
+                    .filter(User.email == request.json['email']) \
+                    .filter(User.password == request.json['password']).first()
+
+    if check_user is not None:
+        response = {
+            "data": "Welcome " + str(check_user.name),
+            "message": "Anda Berhasil Login",
+            "status": 'SUCCESS_LOGIN'
+        }
 
     return response
